13e25e68ee587702b94064a275c79ccfb6d0fde3
kenfus <vincenzo.timmel@students.fhnw.ch>
2022-05-11 01:30:04 +0200

    Changed logic of scripts.

--- numerdash_app.py
+++ numerdash_app.py
@@ -832,8 +832,8 @@ def show_content():
 # main body
 # various configuration setting
 app_opt = {
-            'stake_overview': 'Stake Overview',
-           'performance_overview' : 'Performance Overview'
+           'performance_overview' : 'Performance Overview',
+           'stake_overview': 'Stake Overview',
            # 'app_setting':''
            }
 
